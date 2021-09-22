d5f46724f00f9553fb8dd8ec62f8425bf40f78ff
nattapon wong <nattapon-w@outlook.com>
2021-09-22 20:57:40 +0700

    upload

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,7 +2,7 @@
     <i class="c-icon c-icon-lg cil-menu"></i>
 </button>
 <a class="c-header-brand d-lg-none c-header-brand-sm-up-center" href="#">
-    <img src="https://infyom.com/images/logo/logo_236w.png" width="118" alt="Brand Logo">
+    <!-- <img src="https://infyom.com/images/logo/logo_236w.png" width="118" alt="Brand Logo"> -->
 </a>
 <button class="c-header-toggler c-class-toggler mfs-3 d-md-down-none" type="button" data-target="#sidebar" data-class="c-sidebar-lg-show" responsive="true">
     <i class="c-icon c-icon-lg cil-menu"></i>
